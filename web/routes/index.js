1f34a7ab322f5b44992d43f6b7587e8d6239ed79
Rodrigo Jiménez <rosec@protonmail.ch>
2015-06-17 18:10:13 -0500

    query and body parser

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,5 +12,17 @@ router.get('/json', function(req, res, next) {
   res.json({ title: 'Express', appName: req.appName, color: 'morado' });
 });
 
+router.get('/queryparser', function(req, res, next) {    
+  res.render('queryparser', { query: JSON.stringify(req.query) });
+});
+
+router.get('/bodyparser', function(req, res, next) {    
+  res.render('bodyparser', { body: '' });
+});
+router.post('/bodyparser', function(req, res, next) {    
+  res.render('bodyparser', { body: JSON.stringify(req.body) });
+});
+
+
 
 module.exports = router;
